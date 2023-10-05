b5f3445e9f9fff9e295215b46146abfe03332f94
mdig0003 <96048719+mdig0003@users.noreply.github.com>
2023-10-05 19:26:56 +1100

    css

---
+++ js/choropleth_house_sold.js
@@ -0,0 +1,6 @@
+var choropleth_house_sold = "js/choropleth/choropleth_house_sold.json";
+vegaEmbed('#choroplethHouseSold', choropleth_house_sold).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
