74abff2bd500d658a23e330468492d03da71bed0
mdig0003 <96048719+mdig0003@users.noreply.github.com>
2023-10-06 23:08:08 +1100

    completed css

--- js/radar.js
+++ js/radar.js
@@ -1,6 +1,6 @@
-var violin = "js/violin/violin.json";
+var radar = "js/radar/radial_plot.json";
 
-vegaEmbed('#violin', violin).then(function(result) {
+vegaEmbed('#radar', radar).then(function(result) {
 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
